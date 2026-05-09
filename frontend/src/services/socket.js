1d34ca60c61c73f165c07e3410bdcd95026f896d
manasvi0071 <manishkalesg@gmail.com>
2026-05-09 17:47:47 +0530

    Configure deployment environment variables

--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,4 +1,5 @@
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || 'http://localhost:5000';
+const socket = io(SOCKET_URL);
 export default socket;
